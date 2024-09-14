2026e579f2b1c03bb6135fc6fe8f4d443e4a7160
Elijah Senior <lijee@Elijahs-MacBook-Pro.local>
2024-09-13 23:08:28 -0700

    Almost forgot to push, working on Python

--- python/exercises.py
+++ python/exercises.py
@@ -13,14 +13,45 @@ def change(amount: int) -> dict[int, int]:
     return counts
 
 
+
+
 # Write your first then lower case function here
+def first_then_lower_case(a: list[str], p: Callable):
+            for string in a:
+                if string == []:
+                     return None
+                elif p(string):
+                    return string.lower()
+            return None 
 
 
 # Write your powers generator here
 
+def powers_generator(base: int, limit: int):
+    power = 0
+    result = 1
+    while result <= limit:
+        yield result
+        power += 1
+        result = base ** power
+
+               
+          
 
 # Write your say function here
-
+def say(word=""):
+    words = []
+    
+    def chainable(next_word=None):
+        if next_word is None:
+            return " ".join(words)
+        words.append(next_word)
+        return chainable
+    
+    if word:
+        words.append(word)
+        
+    return chainable
 
 # Write your line count function here
 
